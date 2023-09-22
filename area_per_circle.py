15c9685d2983d17cbde1e9dc941772297c164766
Julien Lamoureux <julien.lamoureux@stu.ocsb.ca>
2023-09-22 13:28:18 +0000

    Added area_per_circle.py

---
+++ area_per_circle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created By: Julien Lamoureux
+# Date: September 22, 2023
+# This program calculates the area and circumference of a circle with a radius of 3
+
+
+import math
+
+
+def main():
+    radius = 3
+    circumference = 2 * math.pi * radius
+    area_of_circle = math.pi * radius**2
+    print("A circle that has a radius of :")
+    print("3 cm")
+    print("")
+    print("Given these dimensions, the Area = {:.2f} cm^2".format(area_of_circle))
+    print("And the Circumference = {:.2f} cm".format(circumference))
+
+
+if __name__ == "__main__":
+    main()
